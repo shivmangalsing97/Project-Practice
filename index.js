b6dc33e592d8d4a29aa4d02ac5134602b7b99152
shivmangalsing97 <shivmangalsing97@gmail.com>
2019-06-26 17:00:47 +0530

    finale

--- index.js
+++ index.js
@@ -1,25 +1,84 @@
-const MongoClient = require('mongodb').MongoClient ;
+const MongoClient = require('mongodb').MongoClient;
+const url = require('url');
 const express = require('express');
 const app = express();
 const PORT = 4001;
 
-const url = "mongodb://localhost:27017/";
+const mongoUrl = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, { useNewUrlParser: true } ,function(err, db) {
+MongoClient.connect(mongoUrl , { useNewUrlParser: true }, function (err, db) {
     if (err) throw err;
     var dbo = db.db('mydb');
 
-    app.get('/users', (req,res)=>{
-        const query = {name : 'shiv' }
-        dbo.collection('User').find(query).toArray(function(err, result) {
+    //Read all users
+    app.get('/users', (req, res) => {
+        dbo.collection('User').find({}).toArray(function (err, result) {
             if (err) throw err;
-           console.log(result);
-           res.status(201).send(result)
-         });
+            res.status(201).send(result)
+        });
+    });
+    //Read singlr user
+    app.get('/users/:name', (req,res)=>{
+        dbo.collection('User').find({}).toArray(function (err, result) {
+            const foundUser = result.find( user => {
+                return user.name === req.params.name 
+            })
+            if(foundUser){
+                res.status(201).send(foundUser);
+            } else {
+                res.status(404).send()
+            }
+        });
+    });
+
+    //Create new User
+    app.post('/create', (req, res) => {
+        const myobj = url.parse(req.url, true).query;
+        dbo.collection('User').insertOne(myobj, function (err) { 
+            return res.send("Created Successfully");
+        });
+    })
+
+    //Update Any User
+    app.put('/update/:name', (req, res, next) => {
+        const myQuery = {name : req.params.name};
+        const queryObject = url.parse(req.url, true).query;
+        const newValue = { $set : queryObject}
+        dbo.collection('User').find({}).toArray(function (err, result) {
+            const foundUser = result.find( user => {
+                return user.name === req.params.name 
+            })
+            if(foundUser){
+                dbo.collection('User').updateOne(myQuery, newValue, function(err) {
+                    if (err) throw err ; 
+                    return res.status(201).send("Updated Successfully");         
+                });
+            } else {
+                res.status(404).send('Not Found')
+            }
+        });
+    });
+
+    //Delete user
+    app.delete('/delete/:name', (req,res)=>{
+        const delobj = {name : req.params.name}
+        dbo.collection('User').find({}).toArray((err, result)=>{
+            const foundUser =result.find( user => {
+                return user.name === req.params.name ;
+            });
+            if(foundUser){
+                dbo.collection('User').deleteOne(delobj, (err) =>{
+                    if(err) throw err;
+                });
+                res.status(201).send('Deleted Successfully');
+            } else {
+                res.status(404).send('Not Found')
+            }
+        })
     })
 
 });
 
-app.listen(PORT,() =>{
+app.listen(PORT, () => {
     console.log(`Server is serving on port :- ${PORT}`)
 });
