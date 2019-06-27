72ab45caecd7761518f1c8a49a7c39c772b9020e
shivmangalsing97 <shivmangalsing97@gmail.com>
2019-06-27 09:37:34 +0530

    jwt token

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.get('/api', (req,res)=>{
+    res.json({
+        message : 'welcome to JWT'
+    })
+});
+
+app.post('/api/posts', verifyToken ,(req,res)=>{
+    jwt.verify(req.token, 'iamasecretkey' , (err, authData)=>{
+        if(err){
+            res.sendStatus(403);
+        } else {
+            res.json({
+                message : '......post created',
+                authData
+            });
+        }
+    })
+    
+});
+
+app.post('/api/login', (req,res)=>{
+    //Mock User
+    const user = {
+        id : 1,
+        username : 'shiv',
+        email : 'shiv@gmail.com'
+    }
+    jwt.sign({user},'iamasecretkey',{expiresIn : '30s'} ,(err, token)=>{
+        res.json({
+            token 
+        })
+    });
+
+});
+
+// format of token
+// Authorization : Bearer <access_token>
+
+//Verify token function 
+function verifyToken(req,res,next){
+    //Get Auth Header Value
+    const bearerHeader = req.headers['authorization'] ; 
+    //check if bearer is not define
+    if(typeof bearerHeader !== 'undefined'){
+        // Split at the space   
+        const bearer = bearerHeader.split(' ');
+        // Get token from array
+        const bearerToken = bearer[1];
+        // Set the token 
+        req.token = bearerToken;
+        // next middleware
+        next();
+    } else {
+        //forbidden 
+        res.sendStatus(403);
+    }
+}
+
+app.listen(4000, ()=>{
+    console.log('server satarted on 4000')
+})
